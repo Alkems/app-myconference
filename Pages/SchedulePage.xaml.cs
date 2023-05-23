29e8f19222a5492ea6314fe615ab32c3d68d55d9
Alkems <mynamezalter@gmail.com>
2023-05-23 14:11:24 +0300

    Rework Schedule page

--- Pages/SchedulePage.xaml.cs
+++ Pages/SchedulePage.xaml.cs
@@ -1,21 +1,41 @@
 namespace app_myconference.Pages;
+public partial class ScheduleDay1Page : SchedulePage
+{
+    public ScheduleDay1Page(ScheduleViewModel vm) : base()
+    {
+        vm.Day = 1;
+        Title = "Schedule - Day 1";
+        BindingContext = vm;
+    }
+}
+
+public partial class ScheduleDay2Page : SchedulePage
+{
+    public ScheduleDay2Page(ScheduleViewModel vm) : base()
+    {
+        vm.Day = 2;
+        Title = "Schedule - Day 2";
+        BindingContext = vm;
+    }
+}
 public partial class SchedulePage : ContentPage
 {
-	readonly ScheduleViewModel vm;
-	public static int Day { get; set; }	
-	public SchedulePage(ScheduleViewModel vm)
-	{
-		InitializeComponent();
+    ScheduleViewModel vm;
+    ScheduleViewModel VM => vm ??= BindingContext as ScheduleViewModel;
+    public SchedulePage()
+    {
+        InitializeComponent();
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        if (VM.Schedule.Count == 0)
+            await VM.LoadDataCommand.ExecuteAsync(null);
+    }
 
-		Day++;
-		vm.Day = Day;
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+    }
 
-		BindingContext = this.vm = vm;
-	}
-	protected override async void OnNavigatedTo(NavigatedToEventArgs args)
-	{
-		base.OnNavigatedTo(args);
-		if (vm.Schedule.Count == 0)
-			await vm.LoadDataCommand.ExecuteAsync(null);
-	}
 }
